979eee383d0b350c65ba2d9e49bdb1736a13e73e
Simon Whitehead <chemnova@gmail.com>
2017-08-02 23:38:32 +1000

    Add Piston and spawn Window at 60fps that runs the emulator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate byteorder;
 extern crate clap;
+extern crate piston_window;
 
 use clap::{App, Arg, SubCommand};
+use piston_window::*;
 
 use std::fs::File;
 use std::io::Read;
@@ -16,15 +18,13 @@ fn main() {
         .version("0.0.0.1")
         .author("Simon Whitehead")
         .about("A GameBoy and GameBoy Colour emulator written in Rust")
-        .arg(
-            Arg::with_name("rom")
-                .short("r")
-                .long("rom")
-                .value_name("ROM_PATH")
-                .required(true)
-                .help("Path to a Gameboy or Gameboy Color ROM")
-                .takes_value(true),
-        )
+        .arg(Arg::with_name("rom")
+            .short("r")
+            .long("rom")
+            .value_name("ROM_PATH")
+            .required(true)
+            .help("Path to a Gameboy or Gameboy Color ROM")
+            .takes_value(true))
         .get_matches();
 
     let rom = matches.value_of("rom").unwrap();
@@ -34,16 +34,37 @@ fn main() {
     println!("Loading game: {}", gameboy.cart_details().game_title);
     let mut now = Instant::now();
 
-    'init: loop {
-        if Instant::now().duration_since(now) > Duration::new(1, 0) {
-            println!("1 second has elapsed (STUB HERE FOR V-BLANK DEBUGGING)");
-            now = Instant::now();
-        }
-        match gameboy.run() {
-            false => break 'init,
-            _ => (),
-        }
+    let opengl = OpenGL::V3_2;
+    let mut window: PistonWindow = WindowSettings::new("piston: image", [300, 300])
+        .exit_on_esc(true)
+        .opengl(opengl)
+        .build()
+        .unwrap();
+
+    window.set_max_fps(60);
+    let mut n = 0;
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            if n > 65 {
+                n = 0;
+            }
+            n += 1;
+            println!("{}", n);
+            clear([1.0; 4], g);
+            gameboy.run()
+        });
     }
+
+    // 'init: loop {
+    // if Instant::now().duration_since(now) > Duration::new(1, 0) {
+    // println!("1 second has elapsed (STUB HERE FOR V-BLANK DEBUGGING)");
+    // now = Instant::now();
+    // }
+    // match  {
+    // false => break 'init,
+    // _ => (),
+    // }
+    // }
 }
 
 fn load_rom(fname: &str) -> std::io::Result<Vec<u8>> {
