13b7c59bd19fa21de7b04b1b9c0de830a7fa478e
Simon Whitehead <chemnova@gmail.com>
2017-08-16 00:05:07 +1000

    Implement 'DEC H'

--- src/gameboy/cpu.rs
+++ src/gameboy/cpu.rs
@@ -176,6 +176,7 @@ impl Cpu {
                 0x22 => self.ld_hli_a(interconnect),
                 0x23 => self.inc_hl(),
                 0x24 => self.inc_h(),
+                0x25 => self.dec_h(),
                 0x26 => self.ld_h_imm8(&operand),
                 0x28 => self.jr_z_imm8(&operand),
                 0x2A => self.ld_a_hli(interconnect),
@@ -723,6 +724,15 @@ impl Cpu {
         self.registers.e = r.wrapping_sub(0x01);
 
         self.registers.flags.zero = self.registers.e == 0x00;
+        self.registers.flags.negative = true;
+        self.registers.flags.half_carry = (r & 0x0F) == 0x00;
+    }
+
+    fn dec_h(&mut self) {
+        let r = self.registers.h;
+        self.registers.h = r.wrapping_sub(0x01);
+
+        self.registers.flags.zero = self.registers.h == 0x00;
         self.registers.flags.negative = true;
         self.registers.flags.half_carry = (r & 0x0F) == 0x00;
     }
